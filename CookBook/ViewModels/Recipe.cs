8c342f3078ab2c57df2aa3f0a69bd5909d30b941
meero <meero@MEEROTT>
2024-02-14 14:07:56 +0700

    Add project files.

---
+++ CookBook/ViewModels/Recipe.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CookBook.ViewModels
+{
+    public class Recipe
+    {
+        public string Name { get; set; } = "";
+        public List<string> Images { get; set; } = [];
+        public List<string> Ingredients { get; set; } = [];
+        public string Instructions { get; set; } = "";
+    }
+
+}
